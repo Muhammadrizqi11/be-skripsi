896f8a9d9bb50e723ca932b3b1bf00d22f114476
Muhammadrizqi11 <mhmmdrizqi2002@gmail.com>
2024-07-24 14:02:07 +0700

    adding-cors

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ import router from "./routes/index.js";
 import bodyParser from "body-parser";
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://fe-skripsi-seven.vercel.app",
+    credentials: true,
+    // tes
+  })
+);
 app.use(express.json());
 
 app.use(router);
