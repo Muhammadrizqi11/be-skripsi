b4f139a545c1e3e186d7adad30f53f0a200da998
Muhammadrizqi11 <mhmmdrizqi2002@gmail.com>
2024-07-24 12:55:55 +0700

    vercel config

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import express from "express";
 import cors from "cors";
 import router from "./routes/index.js";
-import bodyParser from 'body-parser';
+import bodyParser from "body-parser";
 
 const app = express();
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors());
 app.use(express.json());
 
 app.use(router);
