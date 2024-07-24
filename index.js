88ee0f3ccb3224143f5812e5cfe176d4d4eab2db
Muhammadrizqi11 <mhmmdrizqi2002@gmail.com>
2024-07-24 14:33:02 +0700

    vercel config

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import bodyParser from "body-parser";
 const app = express();
 app.use(
   cors({
-    origin: "https://fe-skripsi-seven.vercel.app",
+    origin: "https://studiobook.netlify.app",
     credentials: true,
   })
 );
